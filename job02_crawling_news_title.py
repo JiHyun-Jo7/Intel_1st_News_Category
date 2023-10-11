bd885b71ccfdae5def85c8f45311696b013ec372
user <jihn2824@gmail.com>
2023-10-11 12:14:54 +0900

    Naver News Crawling Code

--- job02_crawling_news_title.py
+++ job02_crawling_news_title.py
@@ -33,21 +33,23 @@ driver.get(url)
 # //*[@id="section_body"]/ul[2]/li[1]/dl/dt[2]/a : 6번째 기사
 # //*[@id="section_body"]/ul[4]/li[5]/dl/dt[2]/a : 20번째 기사
 # //*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a : 2페이지 1번째 기사
-# 이중 for문을 사용하여 전체 기사 제목 출력
+# 다중 for문을 사용하여 전체 기사 제목 출력
 
 category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
+# # 각 카테고리 별 총 페이지 수
 # pages = [146, 328, 461, 75, 117, 72]
-pages = [110, 110, 110, 75, 110, 72]
 # # 학습을 위해 최대 페이지를 중간 지점인 110 페이지로 제한함
+pages = [110, 110, 110, 75, 110, 72]
 df_titles = pd.DataFrame()
 
 for i in range(6):
+    # 카테고리 변경
     section_url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}'.format(i)
     titles = []                 # titles 초기화
-    for j in range(1, 3):       # pages[i]+1
-        url = section_url + '#&date=%2000:00:00&page={}'.format(j)
+    for j in range(1, 3):       # pages[i]+1 (시간 문제 상 3으로 축소)
+        url = section_url + '#&date=%2000:00:00&page={}'.format(j)        # 페이지 변경
         driver.get(url)
-        time.sleep(0.5)           # 페이지가 바뀔 시간을 줌 (1초 미만 권장)
+        time.sleep(0.5)           # 페이지가 바뀔 수 있도록 대기 시간을 줌 (1초 미만 권장)
         for k in range(1, 5):
             for m in range(1, 6):
                 title = driver.find_element('xpath', '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(k, m)).text
@@ -56,7 +58,7 @@ for i in range(6):
                 print(title)        # 기사 제목 출력
     df_section_title = pd.DataFrame(titles, columns=['titles'])
     df_section_title['category'] = category[i]
-    df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+    df_titles = pd.concat([df_titles, df_section_title], ignore_index=False)
 df_titles.to_csv('./crawling_data/crawling_data.cvs')
 
 print(df_titles.head())

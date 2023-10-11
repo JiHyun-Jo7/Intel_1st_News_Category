49ba3834b570a2ed60415ceb9425bf357af1718c
user <jihn2824@gmail.com>
2023-10-11 15:22:40 +0900

    Naver News Crawling Code

--- job02_crawling_news_title.py
+++ job02_crawling_news_title.py
@@ -23,10 +23,8 @@ options.add_argument('lang=ko_KR')          # 언어 설정
 
 # 크롬 드라이버 최신 버전 설정
 service = ChromeService(executble_path=ChromeDriverManager().install())
-
 # 크롬 드라이버
 driver = webdriver.Chrome(service=service, options=options)
-driver.get(url)
 
 # //*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a  : X path 복사
 # //*[@id="section_body"]/ul[1]/li[2]/dl/dt[2]/a : 2번째 기사
@@ -36,10 +34,8 @@ driver.get(url)
 # 다중 for문을 사용하여 전체 기사 제목 출력
 
 category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
-# # 각 카테고리 별 총 페이지 수
-# pages = [146, 328, 461, 75, 117, 72]
-# # 학습을 위해 최대 페이지를 중간 지점인 110 페이지로 제한함
-pages = [110, 110, 110, 75, 110, 72]
+# pages = [146, 328, 461, 75, 117, 72]  # 각 카테고리 별 총 페이지 수
+pages = [110, 110, 110, 75, 110, 72]    # 학습을 위해 최대 페이지를 중간 지점인 110 페이지로 제한함
 df_titles = pd.DataFrame()
 
 for i in range(6):
@@ -52,15 +48,23 @@ for i in range(6):
         time.sleep(0.5)           # 페이지가 바뀔 수 있도록 대기 시간을 줌 (1초 미만 권장)
         for k in range(1, 5):
             for m in range(1, 6):
-                title = driver.find_element('xpath', '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(k, m)).text
-                title = re.compile('[^가-힣]').sub(' ', title)
-                titles.append(title)
-                print(title)        # 기사 제목 출력
+                try:
+                    title = driver.find_element('xpath', '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(k, m)).text
+                    title = re.compile('[^가-힣]').sub(' ', title)
+                    titles.append(title)
+                except:
+                    print('error {} {} {} {}'.format(i, j, k, m))
+        if j % 10 == 0:
+            df_section_title = pd.DataFrame(titles,columns=['titles'])
+            df_section_title['category']=category[i]
+            df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+            df_titles.to_csv('./crawling_data/naver_news_{}_{}.csv'.format(i, j), index=False)
+            titles = []
     df_section_title = pd.DataFrame(titles, columns=['titles'])
     df_section_title['category'] = category[i]
-    df_titles = pd.concat([df_titles, df_section_title], ignore_index=False)
-df_titles.to_csv('./crawling_data/crawling_data.cvs')
+    df_titles = pd.concat([df_titles, df_section_title], ignore_index=True)
+    df_titles.to_csv('./crawling_data/crawling_data_last.csv', index=False)
 
-print(df_titles.head())
+print(df_titles.head(20))       # 상위 제목 20개 출력
 df_titles.info()
 print(df_titles['category'].value_counts())

5b008eb3647dfb3288dc9a95af221d2c3463bc37
user <jihn2824@gmail.com>
2023-10-12 18:29:28 +0900

    Model Learning

--- job05_model_learning.py
+++ job05_model_learning.py
@@ -4,12 +4,12 @@ from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 
 X_train, X_test, Y_train, Y_test = np.load(
-    './crawling_data/news_data_max_21_wordsize_12962.npy', allow_pickle=True)
+    './crawling_data/news_data_max_23_wordsize_16374.npy', allow_pickle=True)
 print(X_train.shape, Y_train.shape)
 print(X_test, Y_test)
 
 model = Sequential()
-model.add(Embedding(12962, 300, input_length=21))
+model.add(Embedding(16374, 300, input_length=23))       # input_length = max
 model.add(Conv1D(32, kernel_size=5, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=1))
 model.add(LSTM(128, activation='tanh', return_sequences=True))
